171b983635b0fa2ec14e3217a99711538c220f5c
Chaahat <u6398806@anu.edu.au>
2017-11-14 17:56:46 +1100

    Test

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -15,21 +16,29 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton mNextButton;
     private ImageButton mPrevButton;
     private TextView mQuestionTextView;
+    private static final String TAG = "MainActivity";
+    private static final String KEY_INDEX = "index";
 
     private Question[] mQuestionBank = new Question[]{
-            new Question(R.string.question_africa,true),
+            new Question(R.string.question_africa, true),
             new Question(R.string.question_oceans, true),
-            new Question(R.string.question_mideast,false),
-            new Question(R.string.question_africa,false),
-            new Question(R.string.question_americas,true),
-            new Question(R.string.question_asia,true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
     };
 
     private int mCurrentIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
@@ -39,35 +48,35 @@ public class MainActivity extends AppCompatActivity {
                                                      updateQuestion();
                                                  }
                                              }
-            );
+        );
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new
-View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        checkAnswer(true);
-    }
-});
+                                               View.OnClickListener() {
+                                                   @Override
+                                                   public void onClick(View v) {
+                                                       checkAnswer(true);
+                                                   }
+                                               });
 
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new
-View.OnClickListener(){
+                                                View.OnClickListener() {
 
-    @Override
-    public void onClick(View v) {
-        checkAnswer(false);
-    }
-});
+                                                    @Override
+                                                    public void onClick(View v) {
+                                                        checkAnswer(false);
+                                                    }
+                                                });
         mNextButton = (ImageButton) findViewById(R.id.next_button);
-            mNextButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                    updateQuestion();
-                }
-            });
-            updateQuestion ();
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+        updateQuestion();
 
         mPrevButton = (ImageButton) findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(new View.OnClickListener() {
@@ -77,22 +86,62 @@ View.OnClickListener(){
                 updateQuestion();
             }
         });
+        updateQuestion();
     }
-    private void updateQuestion () {
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSavedInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    private void updateQuestion() {
+        Log.d(TAG, "Updating question text", new Exception());
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
     }
-    private void checkAnswer(boolean userPressedTrue)
-    {
+
+    private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
         int messageResId = 0;
 
         if (userPressedTrue == answerIsTrue) {
-            messageResId =  R.string.correct_toast;
+            messageResId = R.string.correct_toast;
         } else {
             messageResId = R.string.incorrect_toast;
         }
-        Toast.makeText(this,messageResId,Toast.LENGTH_SHORT)
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
                 .show();
     }
 }
